6f61447f2a880ada05d0690e7e566fa7655c4fb1
Moisés López <moylop260@vauxoo.com>
2022-10-16 15:12:07 -0500

    enable-testing

---
+++ tests/test_checks.py
@@ -0,0 +1,9 @@
+import unittest
+# import checks_odoo_module
+# from oca_pre_commit_hooks.checks_odoo_module import run
+
+class TestChecks(unittest.TestCase):
+    def test_checks(self):
+        # run()
+        import pdb;pdb.set_trace()
+        print("Hola mundo!")
