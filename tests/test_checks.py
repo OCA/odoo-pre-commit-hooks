00f382e88fb2692f36266fc341788a7943e7cfd3
Moisés López <moylop260@vauxoo.com>
2022-10-16 15:54:10 -0500

    test-fix

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -4,11 +4,17 @@ import unittest
 
 import oca_pre_commit_hooks
 
+ALL_CHECKS = [
+    "xml_deprecated_openerp_xml_node",
+    "xml_deprecated_qweb_directive",
+    "xml_not_valid_char_link",
+    "xml_create_user_wo_reset_password",
+]
+
 
 class TestChecks(unittest.TestCase):
     def test_checks(self):
-        manifest_paths = set(glob.glob("./test_repo/*/__openerp__.py") + glob.glob("./test_repo/*/__manifest__.py"))
+        manifest_paths = glob.glob("./test_repo/*/__openerp__.py") + glob.glob("./test_repo/*/__manifest__.py")
         check_errors = oca_pre_commit_hooks.checks_odoo_module.run(manifest_paths, do_exit=False, verbose=False)
-        check_errors_keys = check_errors.keys()
-        # import pdb;pdb.set_trace()
-        print("Hola mundo!")
+        check_errors_keys = list(check_errors.keys())
+        self.assertEqual(ALL_CHECKS, check_errors_keys)
