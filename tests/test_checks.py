92940c3400b7a9131c8482fe03fb21040027fe77
Moisés López <moylop260@vauxoo.com>
2022-10-16 15:42:56 -0500

    borrar

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,9 +1,14 @@
+import glob
 import unittest
+
 
 import oca_pre_commit_hooks
 
 
 class TestChecks(unittest.TestCase):
     def test_checks(self):
-        check_errors = oca_pre_commit_hooks.checks_odoo_module.run(["test_repo"], do_exit=False, verbose=False)
+        manifest_paths = set(glob.glob("./test_repo/*/__openerp__.py") + glob.glob("./test_repo/*/__manifest__.py"))
+        check_errors = oca_pre_commit_hooks.checks_odoo_module.run(manifest_paths, do_exit=False, verbose=False)
+        check_errors_keys = check_errors.keys()
+        # import pdb;pdb.set_trace()
         print("Hola mundo!")
