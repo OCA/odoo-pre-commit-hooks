OCA/odoo-pre-commit-hooks

commit d7753c87378aa26c3bccc55742487f89f595d72c

[REF] Don't filter out messages, move is_message_enabled
Related to #77.

* Messages are no longer filtered out. Instead they are not generated in
  the first place. This prevents subtle bugs from being introduced,
  where a check is run (unwanted) and no one notices it because its
  messages are filtered out. It also simplifies the program's flow and
  avoids useless execution.

* is_message_enabled has been moved to BaseChecker, this makes code
  easier and removes redundancy since the same enable, disable sets were
  being passed around constantly.

src/oca_pre_commit_hooks/base_checker.py
@@ -1,5 +1,5 @@
 from collections import defaultdict
-from typing import Set
+from typing import Set, Union
 
 
 class BaseChecker:
@@ -11,3 +11,13 @@ class BaseChecker:
 
         self.checks_errors = defaultdict(list)
         self.needs_autofix = False
+
+    def is_message_enabled(self, message: str, extra_disable: Union[Set, None] = None):
+        if extra_disable:
+            return message not in extra_disable
+        if self.enable:
+            return message in self.enable
+        if self.disable:
+            return message not in self.disable
+
+        return True

src/oca_pre_commit_hooks/checks_odoo_module.py
@@ -129,7 +129,7 @@ class ChecksOdooModule(BaseChecker):
         checks_obj = checks_odoo_module_xml.ChecksOdooModuleXML(
             manifest_datas, self.odoo_addon_name, self.enable, self.disable
         )
-        for check_meth in utils.getattr_checks(checks_obj, self.enable, self.disable):
+        for check_meth in utils.getattr_checks(checks_obj):
             check_meth()
         self.checks_errors.update(checks_obj.checks_errors)
 
@@ -141,7 +141,7 @@ class ChecksOdooModule(BaseChecker):
         checks_obj = checks_odoo_module_csv.ChecksOdooModuleCSV(
             manifest_datas, self.odoo_addon_name, self.enable, self.disable
         )
-        for check_meth in utils.getattr_checks(checks_obj, self.enable, self.disable):
+        for check_meth in utils.getattr_checks(checks_obj):
             check_meth()
         self.checks_errors.update(checks_obj.checks_errors)
 
@@ -196,9 +196,8 @@ def run(files_or_modules, enable=None, disable=None, no_verbose=False, no_exit=F
         checks_obj = ChecksOdooModule(
             os.path.realpath(manifest_path), enable, disable, changed=changed, verbose=not no_verbose, autofix=autofix
         )
-        for check in utils.getattr_checks(checks_obj, enable=enable, disable=disable):
+        for check in utils.getattr_checks(checks_obj):
             check()
-        utils.filter_checks_enabled_disabled(checks_obj.checks_errors, enable, disable)
         if checks_obj.checks_errors:
             all_check_errors.append(checks_obj.checks_errors)
             exit_status = 1

src/oca_pre_commit_hooks/checks_odoo_module_csv.py
@@ -1,16 +1,23 @@
 import csv
 import os
 from collections import defaultdict
+from typing import Sequence, Set, Union
 
 from oca_pre_commit_hooks import utils
 from oca_pre_commit_hooks.base_checker import BaseChecker
 
 
 class ChecksOdooModuleCSV(BaseChecker):
-    def __init__(self, manifest_datas, module_name, enable, disable):
+    def __init__(
+        self,
+        manifest_datas: Union[Sequence, None] = None,
+        module_name: str = None,
+        enable: Union[Set, None] = None,
+        disable: Union[Set, None] = None,
+    ):
         super().__init__(enable, disable, module_name)
 
-        self.manifest_datas = manifest_datas
+        self.manifest_datas = manifest_datas or []
         for manifest_data in manifest_datas:
             manifest_data.update(
                 {
@@ -44,12 +51,15 @@ class ChecksOdooModuleCSV(BaseChecker):
                             )
                         )
             except (FileNotFoundError, csv.Error, UnicodeDecodeError) as csv_err:
-                self.checks_errors["csv-syntax-error"].append(f'{manifest_data["filename_short"]}:1 {csv_err}')
-        for csvid, records in csvids.items():
-            if len(records) < 2:
-                continue
-            self.checks_errors["csv-duplicate-record-id"].append(
-                f"{records[0][0]}:{records[0][1]} "
-                f'Duplicate CSV record id "{csvid}" in '
-                f'{", ".join(f"{record[0]}:{record[1]}" for record in records[1:])}'
-            )
+                if self.is_message_enabled("csv-syntax-error"):
+                    self.checks_errors["csv-syntax-error"].append(f'{manifest_data["filename_short"]}:1 {csv_err}')
+
+        if self.is_message_enabled("csv-duplicate-record-id"):
+            for csvid, records in csvids.items():
+                if len(records) < 2:
+                    continue
+                self.checks_errors["csv-duplicate-record-id"].append(
+                    f"{records[0][0]}:{records[0][1]} "
+                    f'Duplicate CSV record id "{csvid}" in '
+                    f'{", ".join(f"{record[0]}:{record[1]}" for record in records[1:])}'
+                )

src/oca_pre_commit_hooks/checks_odoo_module_po.py
@@ -247,11 +247,12 @@ class ChecksOdooModulePO(BaseChecker):
         """
         # po_requires_module
         # Regex from https://github.com/odoo/odoo/blob/fa4f36bb631e82/odoo/tools/translate.py#L616  # noqa
-        match = re.match(r"(module[s]?): (\w+)", entry.comment)
-        if not match:
-            self.checks_errors["po-requires-module"].append(
-                f"{self.filename_short}:{entry.linenum} " "Translation entry requires comment `#. module: MODULE`"
-            )
+        if self.is_message_enabled("po-requires-module"):
+            match = re.match(r"(module[s]?): (\w+)", entry.comment)
+            if not match:
+                self.checks_errors["po-requires-module"].append(
+                    f"{self.filename_short}:{entry.linenum} " "Translation entry requires comment `#. module: MODULE`"
+                )
 
         # po_msgstr_variables
         if entry.msgstr and "python-format" in entry.flags:
@@ -262,19 +263,21 @@ class ChecksOdooModulePO(BaseChecker):
                 self.parse_printf(entry.msgid, entry.msgstr)
                 self.parse_format(entry.msgid, entry.msgstr)
             except PrintfStringParseError as str_parse_exc:
-                linenum = self._get_po_line_number(entry)
-                self.checks_errors["po-python-parse-printf"].append(
-                    f"{self.filename_short}:{linenum} "
-                    "Translation string couldn't be parsed "
-                    f"correctly using str%variables {str_parse_exc}"
-                )
+                if self.is_message_enabled("po-python-parse-printf"):
+                    linenum = self._get_po_line_number(entry)
+                    self.checks_errors["po-python-parse-printf"].append(
+                        f"{self.filename_short}:{linenum} "
+                        "Translation string couldn't be parsed "
+                        f"correctly using str%variables {str_parse_exc}"
+                    )
             except FormatStringParseError as str_parse_exc:
-                linenum = self._get_po_line_number(entry)
-                self.checks_errors["po-python-parse-format"].append(
-                    f"{self.filename_short}:{linenum} "
-                    "Translation string couldn't be parsed "
-                    f"correctly using str.format {str_parse_exc}"
-                )
+                if self.is_message_enabled("po-python-parse-format"):
+                    linenum = self._get_po_line_number(entry)
+                    self.checks_errors["po-python-parse-format"].append(
+                        f"{self.filename_short}:{linenum} "
+                        "Translation string couldn't be parsed "
+                        f"correctly using str.format {str_parse_exc}"
+                    )
 
     def check_po(self):
         """* Check po-duplicate-message-definition (message-id)
@@ -295,27 +298,28 @@ class ChecksOdooModulePO(BaseChecker):
 
             # po_duplicate_message_definition
             duplicated[hash(entry.msgid)].append(entry)
-            for meth in utils.getattr_checks(self, self.enable, self.disable, "visit_entry"):
+            for meth in utils.getattr_checks(self, "visit_entry"):
                 meth(entry)
 
-        for entries in duplicated.values():
-            if len(entries) < 2:
-                continue
-            linenum = self._get_po_line_number(entries[0])
-            duplicated_str = ", ".join(map(str, map(self._get_po_line_number, entries[1:])))
-            msg_id_short = re.sub(r"[\n\t]*", "", entries[0].msgid[:40]).strip()
-            if len(entries[0].msgid) > 40:
-                msg_id_short = f"{msg_id_short}..."
-            self.checks_errors["po-duplicate-message-definition"].append(
-                f"{self.filename_short}:{linenum} "
-                f'Duplicate PO message definition "{msg_id_short}" '
-                f"in lines {duplicated_str}"
-            )
+        if self.is_message_enabled("po-duplicate-message-definition"):
+            for entries in duplicated.values():
+                if len(entries) < 2:
+                    continue
+                linenum = self._get_po_line_number(entries[0])
+                duplicated_str = ", ".join(map(str, map(self._get_po_line_number, entries[1:])))
+                msg_id_short = re.sub(r"[\n\t]*", "", entries[0].msgid[:40]).strip()
+                if len(entries[0].msgid) > 40:
+                    msg_id_short = f"{msg_id_short}..."
+                self.checks_errors["po-duplicate-message-definition"].append(
+                    f"{self.filename_short}:{linenum} "
+                    f'Duplicate PO message definition "{msg_id_short}" '
+                    f"in lines {duplicated_str}"
+                )
 
     def run_checks(self, no_verbose):
-        for check in utils.getattr_checks(self, enable=self.enable, disable=self.disable):
+        for check in utils.getattr_checks(self):
             check()
-        utils.filter_checks_enabled_disabled(self.checks_errors, self.enable, self.disable)
+
         for check_error, msgs in self.checks_errors.items() if not no_verbose else []:
             print(f"\n****{check_error}****")
             for msg in msgs:

src/oca_pre_commit_hooks/checks_odoo_module_xml.py
@@ -54,7 +54,7 @@ class ChecksOdooModuleXML(BaseChecker):
 
     def __init__(self, manifest_datas, module_name, enable, disable):
         super().__init__(enable, disable, module_name)
-        self.manifest_datas = manifest_datas
+        self.manifest_datas = manifest_datas or []
         for manifest_data in self.manifest_datas:
             try:
                 with open(manifest_data["filename"], "rb") as f_xml:
@@ -90,11 +90,7 @@ class ChecksOdooModuleXML(BaseChecker):
 
     def getattr_checks(self, manifest_data, prefix):
         disable_node = manifest_data["disabled_checks"]
-        yield from utils.getattr_checks(self, self.enable, self.disable, prefix, disable_node)
-
-    def is_message_enabled(self, checks, manifest_data):
-        disable_node = manifest_data["disabled_checks"]
-        return utils.is_message_enabled(checks, self.enable, self.disable, disable_node)
+        yield from utils.getattr_checks(self, prefix, disable_node)
 
     @classmethod
     def _get_priority(cls, view):
@@ -136,7 +132,7 @@ class ChecksOdooModuleXML(BaseChecker):
             for record in self.xpath_record_wid(manifest_data["node"]):
                 record_id = record.get("id")
 
-                if self.is_message_enabled("xml-duplicate-record-id", manifest_data):
+                if self.is_message_enabled("xml-duplicate-record-id", manifest_data["disabled_checks"]):
                     # xmlids_duplicated
                     xmlid_key = (
                         f"{manifest_data['data_section']}/{record_id}"
@@ -145,7 +141,7 @@ class ChecksOdooModuleXML(BaseChecker):
                     xmlids_section[xmlid_key].append((manifest_data, record))
 
                 # fields_duplicated
-                if self.is_message_enabled("xml-duplicate-fields", manifest_data):
+                if self.is_message_enabled("xml-duplicate-fields", manifest_data["disabled_checks"]):
                     for field in self.xpath_record_fields_wname(record):
                         xml_fields[(field.get("name"), field.getparent())].append((manifest_data, field))
 
@@ -219,24 +215,26 @@ class ChecksOdooModuleXML(BaseChecker):
         if record.get("model") != "ir.ui.view":
             return
         # view_dangerous_replace_low_priority
-        priority = self._get_priority(record)
-        is_replaced_field = self._is_replaced_field(record)
-        # TODO: Add self.config.min_priority instead of DFTL_MIN_PRIORITY
-        if is_replaced_field and priority < DFTL_MIN_PRIORITY:
-            self.checks_errors["xml-view-dangerous-replace-low-priority"].append(
-                f'{manifest_data["filename_short"]}:{record.sourceline} '
-                'Dangerous use of "replace" from view '
-                f"with priority {priority} < {DFTL_MIN_PRIORITY}. "
-                'Only replace as a last resort. Try position="attributes", position="move" or invisible="1" first'
-            )
+        if self.is_message_enabled("xml-view-dangerous-replace-low-priority", manifest_data["disabled_checks"]):
+            priority = self._get_priority(record)
+            is_replaced_field = self._is_replaced_field(record)
+            # TODO: Add self.config.min_priority instead of DFTL_MIN_PRIORITY
+            if is_replaced_field and priority < DFTL_MIN_PRIORITY:
+                self.checks_errors["xml-view-dangerous-replace-low-priority"].append(
+                    f'{manifest_data["filename_short"]}:{record.sourceline} '
+                    'Dangerous use of "replace" from view '
+                    f"with priority {priority} < {DFTL_MIN_PRIORITY}. "
+                    'Only replace as a last resort. Try position="attributes", position="move" or invisible="1" first'
+                )
 
         # deprecated_tree_attribute
-        for deprecate_attr_node in self.xpath_tree_deprecated(record):
-            deprecate_attr_str = ",".join(set(deprecate_attr_node.attrib.keys()) & self.tree_deprecate_attrs)
-            self.checks_errors["xml-deprecated-tree-attribute"].append(
-                f'{manifest_data["filename_short"]}:{deprecate_attr_node.sourceline} '
-                f'Deprecated "<tree {deprecate_attr_str}=..."'
-            )
+        if self.is_message_enabled("xml-deprecated-tree-attribute", manifest_data["disabled_checks"]):
+            for deprecate_attr_node in self.xpath_tree_deprecated(record):
+                deprecate_attr_str = ",".join(set(deprecate_attr_node.attrib.keys()) & self.tree_deprecate_attrs)
+                self.checks_errors["xml-deprecated-tree-attribute"].append(
+                    f'{manifest_data["filename_short"]}:{deprecate_attr_node.sourceline} '
+                    f'Deprecated "<tree {deprecate_attr_str}=..."'
+                )
 
     @utils.only_required_for_checks("xml-create-user-wo-reset-password")
     def visit_xml_record_user(self, manifest_data, record):
@@ -277,7 +275,7 @@ class ChecksOdooModuleXML(BaseChecker):
         """* Check xml-not-valid-char-link
         The resource in in src/href contains a not valid character."""
         for manifest_data in self.manifest_datas:
-            if not self.is_message_enabled("xml-not-valid-char-link", manifest_data):
+            if not self.is_message_enabled("xml-not-valid-char-link", manifest_data["disabled_checks"]):
                 continue
 
             for node in self.xpath_char_links(manifest_data["node"]):
@@ -294,7 +292,9 @@ class ChecksOdooModuleXML(BaseChecker):
         """* Check xml-dangerous-qweb-replace-low-priority
         Dangerous qweb view defined with low priority"""
         for manifest_data in self.manifest_datas:
-            if not self.is_message_enabled("xml-dangerous-qweb-replace-low-priority", manifest_data):
+            if not self.is_message_enabled(
+                "xml-dangerous-qweb-replace-low-priority", manifest_data["disabled_checks"]
+            ):
                 continue
             for template in self.xpath_template(manifest_data["node"]):
                 try:
@@ -317,7 +317,7 @@ class ChecksOdooModuleXML(BaseChecker):
         """* Check xml-deprecated-data-node
         Deprecated <data> node inside <odoo> xml node"""
         for manifest_data in self.manifest_datas:
-            if not self.is_message_enabled("xml-deprecated-data-node", manifest_data):
+            if not self.is_message_enabled("xml-deprecated-data-node", manifest_data["disabled_checks"]):
                 continue
             for data_node in self.xpath_deprecated_data(manifest_data["node"]):
                 # TODO: Add autofix option
@@ -332,7 +332,7 @@ class ChecksOdooModuleXML(BaseChecker):
         """* Check xml-deprecated-openerp-node
         deprecated <openerp> xml node"""
         for manifest_data in self.manifest_datas:
-            if not self.is_message_enabled("xml-deprecated-openerp-node", manifest_data):
+            if not self.is_message_enabled("xml-deprecated-openerp-node", manifest_data["disabled_checks"]):
                 continue
             for openerp_node in self.xpath_openerp(manifest_data["node"]):
                 # TODO: Add autofix option
@@ -345,7 +345,7 @@ class ChecksOdooModuleXML(BaseChecker):
         """* Check xml-deprecated-qweb-directive
         for use of deprecated QWeb directives t-*-options"""
         for manifest_data in self.manifest_datas:
-            if not self.is_message_enabled("xml-deprecated-qweb-directive", manifest_data):
+            if not self.is_message_enabled("xml-deprecated-qweb-directive", manifest_data["disabled_checks"]):
                 continue
             for node in self.xpath_qweb_deprecated(manifest_data["node"]):
                 directive_str = ", ".join(set(node.attrib) & self.qweb_deprecated_directives)

src/oca_pre_commit_hooks/utils.py
@@ -4,8 +4,11 @@ import subprocess
 import sys
 from contextlib import contextmanager
 from functools import lru_cache
+from inspect import getmembers, isfunction
 from itertools import chain
 from pathlib import Path
+
+from oca_pre_commit_hooks.base_checker import BaseChecker
 
 CHECKS_DISABLED_REGEX = re.compile(re.escape("oca-hooks:disable=") + r"([a-z\-,]+)")
 DEPRECATED_CHECKS_DISABLED_REGEX = re.compile(re.escape("pylint:disable=") + r"([a-z\-,]+)")
@@ -55,36 +58,18 @@ def only_required_for_installable():
     return store_installable
 
 
-def is_message_enabled(msg_code, enable, disable, disable_node=None):
-    if disable_node and msg_code in disable_node:
-        # If the check is disabled in the node so return early
-        # because of it could be enabled for other files except this one
-        return False
-    if disable:
-        return msg_code not in disable
-    if enable:
-        return msg_code in enable
-    return True
-
-
-def getattr_checks(obj_or_class, enable=None, disable=None, prefix="check_", disable_node=None):
+def getattr_checks(obj_or_class: BaseChecker, prefix="check_", disable_node=None):
     """Get all the attributes callables (methods)
     that start with word 'def check_*'
     Skip the methods with attribute "checks" defined if
     the check is not enable or if it is disabled"""
-    if enable is None:
-        enable = set()
-    if disable is None:
-        disable = set()
-    if disable_node is None:
-        disable_node = set()
     for attr in dir(obj_or_class):
         if not callable(getattr(obj_or_class, attr)) or not attr.startswith(prefix):
             continue
         meth = getattr(obj_or_class, attr)
         meth_checks = getattr(meth, "checks", set())
         if meth_checks and not any(
-            is_message_enabled(meth_check, enable, disable, disable_node) for meth_check in meth_checks
+            obj_or_class.is_message_enabled(meth_check, disable_node) for meth_check in meth_checks
         ):
             continue
         meth_installable = getattr(meth, "installable", None)
@@ -152,31 +137,13 @@ def walk_up(path, filenames, top):
     return walk_up(os.path.dirname(path), filenames, top)
 
 
-def filter_checks_enabled_disabled(checks_errors, enables, disables):
-    """Remove disabled checks from "check_errors" dictionary
-    It is needed since that there are checks called without option to disable them
-    e.g. syntax error checks
-
-    Remove checks not enabled only if enabled was defined
-    """
-    if not checks_errors:
-        return
-    for disable in disables or []:
-        checks_errors.pop(disable, False)
-    if not enables:
-        return
-    checks_no_enable = set(checks_errors) - enables
-    for check_no_enable in checks_no_enable:
-        checks_errors.pop(check_no_enable, False)
-
-
 def get_checks_docstring(check_classes):
     checks_docstring = ""
     checks_found = set()
     for check_class in check_classes:
         check_meths = chain(
-            getattr_checks(check_class, prefix="visit"),
-            getattr_checks(check_class, prefix="check"),
+            [member[1] for member in getmembers(check_class, predicate=isfunction) if member[0].startswith("check")],
+            [member[1] for member in getmembers(check_class, predicate=isfunction) if member[0].startswith("visit")],
         )
         # Sorted to avoid mutable checks order readme
         check_meths = sorted(

commit 3c7156634a7420974db5825f2a515046a626a25d

[FIX] unwanted execution of globally disabled checks
If some checks were disabled inline on a file, the global disabled checks were
ignored and possibly executed.

This is likely a regression since d7753c87, where the `is_message_enabled` was
refactorized and this condition was changed.

src/oca_pre_commit_hooks/base_checker.py
@@ -13,8 +13,8 @@ class BaseChecker:
         self.needs_autofix = False
 
     def is_message_enabled(self, message: str, extra_disable: Union[Set, None] = None):
-        if extra_disable:
-            return message not in extra_disable
+        if extra_disable and message in extra_disable:
+            return False
         if self.enable:
             return message in self.enable
         if self.disable:

test_repo/broken_module/skip_xml_check_3.xml
@@ -11,5 +11,14 @@
             <field name="name">view.model.form80</field>
             <field name="model">test.model</field>
         </record>
+        <!-- the check xml-dangerous-view-replace-low-priority is not disabled, so it must fail -->
+        <record id="view_model_skip_but_checked" model="ir.ui.view">
+            <field name="name">view.model.form80</field>
+            <field name="model">test.model</field>
+            <field name="inherit_id" ref="module.template"/>
+            <field name="arch" type="xml">
+                <field name="address" position="replace"/>
+            </field>
+        </record>
     </data>
 </odoo>

tests/test_checks.py
@@ -33,7 +33,7 @@ EXPECTED_ERRORS = {
     "xml-not-valid-char-link": 2,
     "xml-redundant-module-name": 1,
     "xml-syntax-error": 2,
-    "xml-view-dangerous-replace-low-priority": 6,
+    "xml-view-dangerous-replace-low-priority": 7,
     "xml-xpath-translatable-item": 4,
     "xml-oe-structure-missing-id": 6,
     "xml-record-missing-id": 2,