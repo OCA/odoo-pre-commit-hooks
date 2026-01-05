cfcbea08b616a5c6ccbd8e6a4afa0c4704af4b0a
Moisés López - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-01-04 23:52:31 -0600

    [REF] prefer-env-translation: Consider static method case (#165)
    Only raises the check if it is a method is using 'self' in the first argument
    
    The "self.env._" could be not available if it is not a method with 'self'

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -24,7 +24,7 @@ EXPECTED_ERRORS = {
     "file-not-used": 1,
     "manifest-superfluous-key": 3,
     "manifest-syntax-error": 2,
-    "prefer-env-translation": 41,
+    "prefer-env-translation": 39,
     "prefer-readme-rst": 1,
     "unused-logger": 1,
     "use-header-comments": 1,
