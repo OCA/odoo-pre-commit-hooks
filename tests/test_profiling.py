3396fdf9915aa99e9d90039af1796d5ff42fa86d
Moisés López - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2026-01-05 19:33:24 -0600

    [REF] tests: Migrating unittest to pytest (#167)
    To be able to run multiple parallels jobs
    
        tox -e py -- -n auto
    
    It allows to run multiple test cases faster

--- tests/test_profiling.py
+++ tests/test_profiling.py
@@ -6,7 +6,8 @@ from os import environ
 from os.path import dirname, join, realpath
 from pstats import Stats
 from sys import stdout
-from unittest import TestCase, skipUnless
+
+import pytest
 
 import oca_pre_commit_hooks
 from . import common
@@ -28,8 +29,8 @@ def cprofile():
     stats.print_stats()
 
 
-@skipUnless(environ.get("PROFILING"), "Profiling not enabled")
-class TestProfiling(TestCase):
+@pytest.mark.skipif(not environ.get("PROFILING"), reason="Profiling not enabled")
+class TestProfiling:
     @staticmethod
     def manifests_from_repo(repo: str):
         return glob(join(repo, "**", "__openerp__.py"), recursive=True) + glob(
@@ -44,7 +45,7 @@ class TestProfiling(TestCase):
         return glob(po_glob_pattern, recursive=True) + glob(pot_glob_pattern, recursive=True)
 
     @classmethod
-    def setUpClass(cls):
+    def setup_class(cls):
         test_repo_path = join(dirname(dirname(realpath(__file__))), "test_repo")
 
         cls.module_files = cls.manifests_from_repo(test_repo_path)
@@ -53,29 +54,39 @@ class TestProfiling(TestCase):
     def test_profile_checks_module(self):
         checks_module_run = oca_pre_commit_hooks.cli.main
         checks_module_run2 = oca_pre_commit_hooks.cli_fixit.main
-        sys.argv = ["", "--no-exit", "--no-verbose"] + self.module_files
-        with cprofile():
-            errors = checks_module_run()
-            errors += checks_module_run2()
 
-        self.assertDictEqual(common.ChecksCommon.get_count_code_errors(errors), MODULES_ERRORS)
+        mp = pytest.MonkeyPatch()
+        mp.setattr(sys, "argv", ["", "--no-exit", "--no-verbose"] + self.module_files)
+        try:
+            with cprofile():
+                errors = checks_module_run()
+                errors += checks_module_run2()
+            common.assertDictEqual(self, common.ChecksCommon.get_count_code_errors(errors), MODULES_ERRORS)
+        finally:
+            mp.undo()
 
     def test_profile_checks_po(self):
         checks_po_run = oca_pre_commit_hooks.cli_po.main
-        sys.argv = ["", "--no-exit", "--no-verbose"] + self.po_files
-        with cprofile():
-            errors = checks_po_run()
+        mp = pytest.MonkeyPatch()
+        mp.setattr(sys, "argv", ["", "--no-exit", "--no-verbose"] + self.po_files)
+        try:
+            with cprofile():
+                errors = checks_po_run()
+            common.assertDictEqual(self, common.ChecksCommon.get_count_code_errors(errors), PO_ERRORS)
+        finally:
+            mp.undo()
 
-        self.assertDictEqual(common.ChecksCommon.get_count_code_errors(errors), PO_ERRORS)
-
-    @skipUnless(environ.get("PROFILING_TEST_REPO"), "No custom repository set for profiling")
+    @pytest.mark.skipif(not environ.get("PROFILING_TEST_REPO"), reason="No custom repository set for profiling")
     def test_profile_checks_module_custom(self):
         manifests = self.manifests_from_repo(environ.get("PROFILING_TEST_REPO"))
         checks_module_run = oca_pre_commit_hooks.cli.main
         checks_module_run2 = oca_pre_commit_hooks.cli_fixit.main
-        sys.argv = ["", "--no-exit", "--no-verbose"] + manifests
-
-        print(f"Running oca-checks-odoo-module on {len(manifests)} manifests")
-        with cprofile():
-            checks_module_run()
-            checks_module_run2()
+        mp = pytest.MonkeyPatch()
+        mp.setattr(sys, "argv", ["", "--no-exit", "--no-verbose"] + manifests)
+        try:
+            print(f"Running oca-checks-odoo-module on {len(manifests)} manifests")
+            with cprofile():
+                checks_module_run()
+                checks_module_run2()
+        finally:
+            mp.undo()
